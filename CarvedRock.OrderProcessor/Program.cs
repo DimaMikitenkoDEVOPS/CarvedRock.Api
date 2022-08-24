9e4cb5b77be381280af45e783e350cd68b6c729b
Dima Mikitenko <dima.mikitenko@outlook.com>
2022-08-25 00:03:52 +0300

    initial commit

---
+++ CarvedRock.OrderProcessor/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog.Events;
+using Serilog;
+using System;
+
+namespace CarvedRock.OrderProcessor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = typeof(Program).Assembly.GetName().Name;
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithProperty("Assemnbly", name)
+                .WriteTo.Seq(serverUrl: "http://host.docker.internal:5341")
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .UseSerilog();
+    }
+}
